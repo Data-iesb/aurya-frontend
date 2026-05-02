b0a60ad1beeb72951261497c017d6a994aa9292d
s33ding <robertomdiniz@protonmail.com>
2026-05-02 17:37:29 -0300

    fix: exclude tts/ from S3 sync

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws s3 sync static/ s3://aurya.dataiesb.com/ --delete
+aws s3 sync static/ s3://aurya.dataiesb.com/ --delete --exclude "tts/*"
 echo "✅ Deployed to http://aurya.dataiesb.com.s3-website-us-east-1.amazonaws.com/"
